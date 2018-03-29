2ac4b6667bbaca0b5903939454b2f91103243c27
Ramin Taghizada <ramintagizade@mail.ru>
2018-03-29 17:37:47 +0400

    update prod

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var path = require("path");
 var compression = require("compression");
+var favicon  = require("serve-favicon");
 
 const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(compression());
 app.use(express.static("dist"));
+app.use(favicon(path.join(__dirname,'dist','favicon.ico')));
 
 app.get("*",function(req,res){
 	res.sendFile(path.join(__dirname,"dist/index.html"));
